02c037797d5e7784815d60e60bab4025d8bfdb65
Kevin Jung <kev.jung.ii@gmail.com>
2021-05-07 21:46:09 -0400

    adds index and show routes to cells

--- minesweeper-backend/app/controllers/cells_controller.rb
+++ minesweeper-backend/app/controllers/cells_controller.rb
@@ -3,4 +3,8 @@ class CellsController < ApplicationController
   def index
     render json: Cell.game_board, except: [:created_at, :updated_at]
   end
+
+  def show
+    render json: Cell.find_by(location: params[:id]), except: [:created_at, :updated_at]
+  end
 end
