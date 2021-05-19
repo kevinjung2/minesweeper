b703a796470692adc597209e747a2db77bfb999e
Kevin Jung <kev.jung.ii@gmail.com>
2021-05-19 15:04:34 -0400

    updates routes to only include necessary routes

--- minesweeper-backend/app/controllers/scores_controller.rb
+++ minesweeper-backend/app/controllers/scores_controller.rb
@@ -4,10 +4,6 @@ class ScoresController < ApplicationController
     render json: Score.leaderboards, include: :user, except: [:created_at, :updated_at]
   end
 
-  def show
-
-  end
-
   def create
     user = User.find_or_create_by(name: params[:user])
     score = Score.create(time: params[:score], user: user)
